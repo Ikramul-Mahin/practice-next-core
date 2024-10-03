9a56d3d7da17018c4eb8d832b32f6494eb046866
ikramul <mac@shamimmac.local>
2024-10-03 19:12:31 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
+'use client'
+import { useState } from "react";
 
 
 export default function Home() {
+  const [count ,setCount]=useState(0)
+  const handleAdd=()=>{
+    const newCount= count+1
+    setCount(newCount)
+  }
   return (
     <div className="h-screen">
-     <h1>alhamdulillah</h1>
+     <h1>alhamdulillah:{count}</h1>
+
+     <button onClick={handleAdd}>add</button>
     </div>
   );
 }
